b200e0c30154d11c3706c5ae2571bfbbf001e9a7
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-09-09 16:06:43 -0500

    fix tests

--- test/test_main.py
+++ test/test_main.py
@@ -9,7 +9,7 @@ from addict import Dict
 from mock import patch
 from unittest.mock import Mock
 
-from scheduler.main import EspaScheduler, get_framework
+from scheduler.main import ESPAFramework
 from scheduler.config import config
 from scheduler.espa import api_connect
 
@@ -20,27 +20,24 @@ class TestMain(unittest.TestCase):
         self.host = "http://localhost:1234"
         self.image = "usgseros/espa-scheduler:latest"
         self.cfg = config()
+        self.cfg['mesos_master'] = "http://127.0.0.1:5050"
         
         m.get(self.host, json={"foo": 1})
         self.api = api_connect({"espa_api": self.host, "task_image": self.image})
-        self.scheduler = EspaScheduler(self.cfg, self.api)
-
-    def test_get_framework(self):
-        framework = get_framework(self.cfg)
-        self.assertEqual(list(framework.keys()), ["user", "id", "name", "hostname", "failover_timeout"])
+        self.framework = ESPAFramework(self.cfg, self.api)
 
     def test__getResource(self):
         resource = Dict()
         resource.name = "cpus"
         resource.scalar.value = 1.0
-        resp = self.scheduler._getResource([resource], "cpus")
+        resp = self.framework._getResource([resource], "cpus")
         self.assertEqual(resp, 1.0)
 
     def test__updateResource(self):
         resource = Dict()
         resource.name = "mem"
         resource.scalar.value = 5120
-        self.scheduler._updateResource([resource], "mem", 1024)
+        self.framework._updateResource([resource], "mem", 1024)
         self.assertEqual(resource.scalar.value, 4096)
 
     def test_acceptOffer(self):
@@ -67,41 +64,43 @@ class TestMain(unittest.TestCase):
         offer_bad = Dict()
         offer_bad.resources = [cpu_resource_good, mem_resource_bad]
 
-        self.assertTrue(self.scheduler.acceptOffer(offer_good))
-        self.assertFalse(self.scheduler.acceptOffer(offer_bad))
+        self.assertTrue(self.framework.acceptOffer(offer_good))
+        self.assertFalse(self.framework.acceptOffer(offer_bad))
 
     def test_core_limit_reached(self):
-        scheduler = self.scheduler
-        scheduler.runningList = ["a", "b", "c"]
-        self.assertFalse(scheduler.core_limit_reached())
+        framework = self.framework
+        framework.runningList = ["a", "b", "c"]
+        self.assertFalse(framework.core_limit_reached())
 
-        scheduler.max_cpus = 2
-        self.assertTrue(scheduler.core_limit_reached())
+        framework.max_cpus = 2
+        self.assertTrue(framework.core_limit_reached())
 
-        scheduler.runningList = []
-        scheduler.max_cpus = 10
+        framework.runningList = []
+        framework.max_cpus = 10
 
     @patch('scheduler.espa.APIServer.mesos_tasks_disabled', lambda i: True)
-    def test_resourceOffer_disabled(self):
+    def test_offer_received_disabled(self):
         driver = Mock()
         offers = [Mock()]
-        resp = self.scheduler.resourceOffers(driver, offers)
+        resp = self.framework.offer_received(offers)
         self.assertFalse(resp.tasks.enabled)
 
     @patch('scheduler.espa.APIServer.mesos_tasks_disabled', lambda i: False)
     @patch('scheduler.espa.APIServer.get_products_to_process', lambda a, b, c: {"products": []})
-    def test_resourceOffer_nowork(self):
+    def test_offer_received_nowork(self):
         driver = Mock()
         offers = [Mock()]
 
-        resp = self.scheduler.resourceOffers(driver, offers)
+        resp = self.framework.offer_received(offers)
         self.assertTrue(resp.tasks.enabled)
         self.assertEqual(resp.work.length, 0)
 
     @patch('scheduler.espa.APIServer.mesos_tasks_disabled', lambda i: False)
     @patch('scheduler.espa.APIServer.get_products_to_process', lambda a, b, c: {"products": [{"orderid": "foo@manchu.com-123", "sceneid": "L8BBCC"}, {"orderid": "foo@manchu.com-123", "sceneid": "L7BBCC"}]})
     @patch('scheduler.espa.APIServer.set_to_scheduled', lambda a, b: True)
-    def test_resourceOffer_work(self):
+    @patch('scheduler.main.ESPAFramework.acceptOffer', lambda a, b: True)
+    @patch('scheduler.task.build', lambda a, b, c, d, e, f, g, h: {'agent_id': {'value': 'foo'}})
+    def test_offer_received_work(self):
         driver = Mock()
 
         cpu_resource_good  = Dict()
@@ -113,27 +112,27 @@ class TestMain(unittest.TestCase):
         mem_resource_good.scalar.value = 10240
         disk_resource_good.name = "disk"
         disk_resource_good.scalar.value = 10240
-        offer_good = Dict()
+        offer_good = Mock()
         offer_good.resources = [cpu_resource_good, mem_resource_good, disk_resource_good]
 
         offers = [offer_good]
 
-        resp = self.scheduler.resourceOffers(driver, offers)
+        resp = self.framework.offer_received(offers)
         self.assertTrue(resp.tasks.enabled)
         self.assertEqual(resp.work.length, 2)
         self.assertEqual(resp.offers.accepted, 1)
 
 
-    def test_statusUpdate(self):
+    def test_status_update(self):
         driver = Mock()
-        update = Dict()
-        update.task_id.value = "orderid_@@@_unitid"
-        update.state = "TASK_RUNNING"
+        update = dict()
+        update['status'] = {'task_id': {'value': "orderid_@@@_unitid"}} 
+        update['status']['state'] = "TASK_RUNNING"
 
-        resp = self.scheduler.statusUpdate(driver, update)
+        resp = self.framework.status_update(update)
 
-        self.assertEqual(resp.status, "healthy")
-        self.assertEqual(resp.task_id, "orderid_@@@_unitid")
-        self.assertEqual(resp.state, update.state)
-        self.assertEqual(resp.list.name, "running")
-        self.assertEqual(resp.list.status, "new")
+        self.assertEqual(resp['status'], "healthy")
+        self.assertEqual(resp['task_id'], "orderid_@@@_unitid")
+        self.assertEqual(resp['state'], update['status']['state'])
+        self.assertEqual(resp['list']['name'], "running")
+        self.assertEqual(resp['list']['status'], "new")
